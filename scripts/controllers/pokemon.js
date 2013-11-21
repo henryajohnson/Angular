27ba1944f4a6d1c6f6d52497fada9e44c54f237f
Michael D'Egidio <mad2144@columbia.edu>
2013-11-21 18:34:42 -0500

    add events

--- scripts/controllers/pokemon.js
+++ scripts/controllers/pokemon.js
@@ -4,4 +4,17 @@ pokemonApp.controller('PokemonCtrl', ['$scope', function($scope) {
   	{name: "Bulbasaur", type: "Grass", status: "OK"},
   	{name: "Squirtle", type: "Water", status: "OK"},
   	{name: "Pikachu", type: "Electric", status: "OK"}];
+  
+  $scope.$watch("pokemon.type", function(){
+  	$scope.$broadcast("pokemonApp.event");
+  }, true);
+
+  $scope.$on("pokemonApp.event", function() {
+  	console.log("An event has been fired!");
+  });
+
+  $scope.clickEvent = function ($scope) {
+  	console.log("I'm clicking!");
+  };
+
 }]);
